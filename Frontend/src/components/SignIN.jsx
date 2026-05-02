f00108824f19b32c8d31295f989466f06d73f4d4
mehra91 <pm7300779625@gmail.com>
2026-05-02 11:45:54 +0530

    add signIn/signUp popUp

--- Frontend/src/components/SignIN.jsx
+++ Frontend/src/components/SignIN.jsx
@@ -2,34 +2,56 @@ import React, { useState } from 'react'
 import { RxCross2 } from "react-icons/rx";
 
 const SignIN = ({ setIsSignIn }) => {
-  const [currentState, setCurrentState] = useState('Login');
+  const [currentState, setCurrentState] = useState('SignIn');
   return (
-    <div className='fixed inset-0 bg-black/50 min-h-full w-full z-10'>
-      <div className='flex  '>
-        <h2>
-          {currentState}
-        </h2>
-        <h2>
-          <RxCross2 onClick={() => setIsSignIn(false)} />
-        </h2>
-      </div>
-      <form>
-        {currentState === 'Signup'
-          ?
-          <input type='text ' placeholder='name or username' required />
-          :
-          <></>
-        }
-        <input type="text" placeholder='Email id' />
-        <input type="password" placeholder='Enter password' />
-        <button>
-          {currentState === 'Signup'
-            ? 'SignUP'
-            : 'Login'
-          }
-        </button>
+    <div className='fixed inset-0 bg-black/90 text-black min-h-full w-full z-10  flex items-center justify-center'>
+      <div className='bg-white h-70 rounded flex flex-col items-center   w-80'>
 
-      </form>
+        <div className='flex items-center  justify-around   h-12 w-full '>
+          <h2 className=' text-xl text-orange-500 font-bold tracking-wider
+            w-2xs h-full flex items-center justify-center'>
+            {currentState}
+          </h2>
+          <h2 className='text-xl text-black font-medium '>
+            <RxCross2 className='cursor-pointer text-orange-500 '
+              onClick={() => setIsSignIn(false)} />
+          </h2>
+        </div>
+        <form className='flex flex-col    h-full w-full gap-2 items-center '>
+          {currentState === 'SignIn'
+            ?
+            <></>
+            :
+            <input type='text ' placeholder='name or username' required className='border rounded h-8 w-3xs capitalize text-xs tracking-wide font-medium text-center' />
+          }
+          <input type="text" placeholder=' Enter your  Email ' required className='border rounded h-8 w-3xs   text-xs text-center tracking-wide font-medium ' />
+          <input type="password" placeholder='Enter password' required className='border rounded h-8 w-3xs    text-xs text-center tracking-wide font-medium ' />
+          <button className='cursor-pointer border h-8 w-3xs rounded text-sm font-medium bg-orange-500 '>
+            {currentState === 'Signup' 
+              ? 'Create account'
+              : 'Login'
+            }
+          </button>
+          <div className='flex items-center justify-center gap-x-2 h-auto w-3xs'>
+            <input type="checkbox" required className='cursor-pointer' />
+            <p className='text-xs opacity-80 text-pretty  font-medium '>
+              By continuing, i agree the terms of use & privacy policy .
+            </p>
+          </div>
+
+          {
+            currentState === 'SignUp'
+              ? <p className='text-xs font-medium opacity-60  '>
+                Already have an account ? <span onClick={() => setCurrentState("SignIn")} className='text-orange-500 opacity-100 font-bold cursor-pointer'>SignIn</span>
+              </p>
+
+              : <p className='text-xs font-medium opacity-60  '>
+                Create a new account ? <span onClick={() => setCurrentState("SignUp")} className='text-orange-500 opacity-100 font-bold cursor-pointer'>Click here</span>
+              </p>
+          }
+
+        </form>
+      </div>
     </div>
   )
 }
