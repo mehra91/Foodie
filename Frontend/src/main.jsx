42e16effd20d12d1a3e25d679fcbd77596e4c7ed
mehra91 <pm7300779625@gmail.com>
2026-05-08 12:49:35 +0530

    //

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import './index.css';
+import './Index.css';
 import App from './App.jsx'
 import { StoreContextProvider } from './Context/StoreContext.jsx';
 
