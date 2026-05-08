6c93d47cb0f13ebee2c3abf23e40676e1f0a862d
Priyanshu Mehra <pm7300779625@gmail.com>
2026-05-08 13:01:43 +0530

    Fix import case for index.css

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import './Index.css';
+import './index.css';
 import App from './App.jsx'
 import { StoreContextProvider } from './Context/StoreContext.jsx';
 
