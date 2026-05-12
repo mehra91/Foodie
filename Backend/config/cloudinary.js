78ceb531597f17aaa1e131dd0f46e8fd796bd6b0
mehra91 <pm7300779625@gmail.com>
2026-05-12 09:22:21 +0530

    fix cloudinary env variable names

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -3,9 +3,9 @@ import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
